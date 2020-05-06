0935f7c76132b79f4cc810b754b187d7380a44ec
BruceleeVaene <hybruflashpoint@gmail.com>
2020-05-06 23:27:45 +0000

    initial HTML and table

---
+++ main.js
@@ -0,0 +1,9 @@
+$.ajax({
+    method: "POST",
+    url: "https://sgt.lfzprototypes.com/api/users",
+    data: {
+        "firstName": "Brucelee",
+        "lastName": "Vaene",
+        "cohort": "c04.20"
+    }
+});
