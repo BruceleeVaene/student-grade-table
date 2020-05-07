e2032868425ecce8e325a8c5937554136417d045
BruceleeVaene <hybruflashpoint@gmail.com>
2020-05-07 00:37:06 +0000

    requesting data successful

--- components/app.js
+++ components/app.js
@@ -1,3 +1,24 @@
 class App {
-    
+    constructor() {
+        this.handleGetGradesError = this.handleGetGradesError.bind(this);
+        this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    }
+    handleGetGradesError(error) {
+        console.error(error);
+    }
+    handleGetGradesSuccess(grades) {
+        console.log(grades);
+    }
+    getGrades() {
+        $.ajax({
+            method: "GET",
+            url: "https://sgt.lfzprototypes.com/api/grades",
+            headers: {"X-Access-Token": "jW44Wi2y"},
+            success: this.handleGetGradesSuccess,
+            error: this.handleGetGradesError
+        });
+    }
+    start() {
+        this.getGrades();
+    }
 }
--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-$.ajax({
-    method: "POST",
-    url: "https://sgt.lfzprototypes.com/api/users",
-    data: {
-        "firstName": "Brucelee",
-        "lastName": "Vaene",
-        "cohort": "c04.20"
-    }
-});
+//$.ajax({
+  //  method: "POST",
+    //url: "https://sgt.lfzprototypes.com/api/users",
+    //data: {
+        //"firstName": "Brucelee",
+        //"lastName": "Vaene",
+        //"cohort": "c04.20"
+   // }
+//});
+
+var application = new App();
+application.start();
