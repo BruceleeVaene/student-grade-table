faf311370ac06d169307226ba68f46b8d57e71e8
BruceleeVaene <hybruflashpoint@gmail.com>
2020-05-07 19:07:27 +0000

    added and linked page-header script

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-//$.ajax({
-  //  method: "POST",
-    //url: "https://sgt.lfzprototypes.com/api/users",
-    //data: {
-        //"firstName": "Brucelee",
-        //"lastName": "Vaene",
-        //"cohort": "c04.20"
-   // }
-//});
-
 var table = document.querySelector('table');
 var gradeTable = new GradeTable(table);
 
