53f71b433d2228d9afc42639c4d11f89cc7bcb22
BruceleeVaene <hybruflashpoint@gmail.com>
2020-05-07 19:34:49 +0000

    instantiated PageHeader

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var table = document.querySelector('table');
+var header =document.querySelector('header');
+
+var pageHeader = new PageHeader(header);
+
 var gradeTable = new GradeTable(table);
 
 var application = new App(gradeTable);
