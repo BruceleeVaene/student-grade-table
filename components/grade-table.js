08302e7e03d3bd5e5613ccd6f20ddcbb5e7f17eb
BruceleeVaene <hybruflashpoint@gmail.com>
2020-05-07 01:20:01 +0000

    dynamic creation and render of grades success

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,21 @@ class GradeTable {
         this.tableElement = tableElement;
     }
     updateGrades(grades) {
-        console.log(grades);
+        var tableBody = this.tableElement.querySelector('tbody');
+        for (var i = 0; i < grades.length; i++) {
+            var row = document.createElement('tr');
+            var name = document.createElement('td');
+            var course = document.createElement('td');
+            var grade = document.createElement('td');
+
+            name.textContent = grades[i].name;
+            course.textContent = grades[i].course;
+            grade.textContent = grades[i].grade;
+
+            row.append(name);
+            row.append(course);
+            row.append(grade);
+            tableBody.append(row);
+        }
     }
 }
